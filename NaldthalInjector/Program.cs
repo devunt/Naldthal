dbb5c0a87140db5e526556fe96f48f427c9c50d2
Bae Junehyeon <devunt@gmail.com>
2018-05-08 22:03:08 +0900

    Update build settings

--- NaldthalInjector/Program.cs
+++ NaldthalInjector/Program.cs
@@ -8,26 +8,21 @@ namespace NaldthalInjector
 {
     static class Program
     {
-        private static BridgeInterface _bridge;
-
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             var process = Process.GetProcessesByName("ffxiv_dx11")[0];
 
-            var dll = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Naldthal.dll");
-
-            _bridge = new BridgeInterface();
-
             string channelName = null;
-            RemoteHooking.IpcCreateServer(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton, _bridge);
+            var bridge = new BridgeInterface();
+            RemoteHooking.IpcCreateServer(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton, bridge);
 
             RemoteHooking.Inject(
-                process.Id,          // ID of process to inject into
-                InjectionOptions.DoNotRequireStrongName,
-                dll,   // 32-bit library to inject (if target is 32-bit)
-                dll,   // 64-bit library to inject (if target is 64-bit)
+                process.Id,
+                InjectionOptions.NoService | InjectionOptions.DoNotRequireStrongName,
+                null,
+                Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "payload.dll"),
                 channelName,
                 Path.GetFullPath("data.json")
             );
